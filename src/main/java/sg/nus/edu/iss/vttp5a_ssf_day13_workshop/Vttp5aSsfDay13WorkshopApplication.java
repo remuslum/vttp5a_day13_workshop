c1259545d78d82cb6897eca0406d96e5b2e13776
remuslum <57741494+remuslum@users.noreply.github.com>
2024-11-25 15:27:02 +0800

    First commit

---
+++ src/main/java/sg/nus/edu/iss/vttp5a_ssf_day13_workshop/Vttp5aSsfDay13WorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp5a_ssf_day13_workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfDay13WorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfDay13WorkshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/edu/iss/vttp5a_ssf_day13_workshop/Vttp5aSsfDay13WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp5a_ssf_day13_workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfDay13WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
